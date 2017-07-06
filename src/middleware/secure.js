b7f20af9291bd516e86ae64167e62c75e6635d95
Grady Rogers <rogersgt819@gmail.com>
2017-07-06 01:20:17 -0400

    security middleware

--- src/middleware/secure.js
+++ src/middleware/secure.js
@@ -1,7 +1,6 @@
 import crypto from 'crypto';
 
 export default function (req, res, next) {
-    // console.log(req.originalUrl);
     switch(req.originalUrl) {
         case '/api/login':
             next();
@@ -10,17 +9,24 @@ export default function (req, res, next) {
             next();
         break;
         default:
+        if (!req.cookies['archeravenue']) forbid(res);
+        else {
             const data = JSON.parse(decrypt(req.cookies['archeravenue']));
             const time = new Date(data.timestamp);
             const now = new Date();
             const timeDiff = now.getDate() - time.getDate();
             if (!time || time > now || timeDiff > 120000000) {
-                res.status(403).send({ Message: 'Please log in first' });
+                forbid(res);
             } else {
                 next();
             }
+        }
         break;
     }
+}
+
+function forbid(res) {
+    res.status(403).send({ Message: 'Please log in first' });
 }
 
 function decrypt(text) {
